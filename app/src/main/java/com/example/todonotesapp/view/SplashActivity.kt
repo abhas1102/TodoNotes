eace127567b3ec9b410fe42df2493a46eed2ea2f
Abhas Kumar <abhas1102@gmail.com>
2022-08-09 15:31:58 +0530

    Implemented notification using Firebase Cloud Messaging service

--- app/src/main/java/com/example/todonotesapp/view/SplashActivity.kt
+++ app/src/main/java/com/example/todonotesapp/view/SplashActivity.kt
@@ -1,13 +1,17 @@
 package com.example.todonotesapp.view
 
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.todonotesapp.R
 import com.example.todonotesapp.utils.SharePrefConst
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class SplashActivity: AppCompatActivity() {
     lateinit var sharedPreferences:SharedPreferences
@@ -18,6 +22,24 @@ class SplashActivity: AppCompatActivity() {
 
         setupSharedPreferences()
         checkLoginStatus()
+        getFcmToken()
+    }
+
+    private fun getFcmToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("SplashActivity", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result.toString()
+
+            // Log and toast
+          //  val msg = getString(R.string.msg_token_fmt, token)
+            Log.d("SplashActivity", token)
+            Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+        })
     }
 
     private fun setupSharedPreferences() {
